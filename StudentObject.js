37f8cf01563935699f4130490ea8f80799de0103
Michelle C. Funk <michelle@michellefunk.com>
2015-04-06 13:04:52 -0500

    Included childhood homes in placesLived array

--- StudentObject.js
+++ StudentObject.js
@@ -2,7 +2,7 @@ var michelle = {
 	name: Michelle,
 	surname: Funk,
 	mbti: {ei: "I", sn: "N", tf: "T", jp: "J"},
-	placesLived: ["Minneapolis, MN", "Corvallis, OR", "Honolulu, HI", "Paris, France", "LaCrosse, WI", "Richmond, IN"],
+	placesLived: ["Minneapolis, MN", "Corvallis, OR", "Honolulu, HI", "Paris, France", "LaCrosse, WI", "Richmond, IN", "Chicago, IL", "Knoxville, TN"],
 	languages: [["English", "fluent"], ["French", "rusty"], ["Python", "n00b"]], ["JavaScript", "beginner"],
 	cat1: {name: "Bandersnatch", type: "domestic shorthair", color: "black", adopted: 2008},
 	bike1: {name: "Babe the Blue", model: "Surly Cross Check", color: "Robin's Egg Blue", ownedSince: 2013, active: true},
