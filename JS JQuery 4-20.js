30d6f04d372211666ed5c9faeeffd6b4647b106e
Michelle C. Funk <michelle@michellefunk.com>
2015-04-20 10:42:13 -0500

    Week 8 notes add 2

---
+++ JS JQuery 4-20.js
@@ -0,0 +1,7 @@
+$(document).ready(function (){
+	alert("Yup, jQuery loaded!");
+});
+//document.ready function: runs as soon as page is ready
+//bling notation is a JQuery specific action saying "I'm doing something with JQuery"
+//In order for this to work: document.ready function must work correctly, 
+//document must be ready, jQuery must be installed
