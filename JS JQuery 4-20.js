34216218825060217b559dc769033eb555b12e4f
Michelle C. Funk <michelle@michellefunk.com>
2015-04-20 11:15:12 -0500

    Updates to wk 8 notes

--- JS JQuery 4-20.js
+++ JS JQuery 4-20.js
@@ -1,6 +1,11 @@
 $(document).ready(function (){
 	alert("Yup, jQuery loaded!");
+	$("#thisId").remove();
 });
+
+//certain CSS operators avialble to JQuery using ""
+
+
 //document.ready function: runs as soon as page is ready
 //bling notation is a JQuery specific action saying "I'm doing something with JQuery"
 //In order for this to work: document.ready function must work correctly, 
